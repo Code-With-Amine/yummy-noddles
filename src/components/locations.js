b42a99ee6bf54a77cd3b743210d70cc371bba953
Code-With-Amine <userihj36@gmail.com>
2023-08-16 13:58:47 +0100

    Change the background of the navBar on scroll

--- src/components/locations.js
+++ src/components/locations.js
@@ -2,19 +2,33 @@ import React from "react";
 import { locations } from "../Data/locationsData";
 
 function Locations() {
+
+  let animateNumber = 0;
+  let locartionClassName = "location location__animate-"
+
+  const setClassName = ()=>{
+    locartionClassName = "location location__animate-"
+    locartionClassName += ++animateNumber
+    return locartionClassName
+  }
   return (
     <>
-    <h1 className="locations--header">Locations</h1>
-    <div className="locations-container">
-  
-      {locations.map(({ cover, title, location }) => 
-        <div className="location">
-          <img src={cover} alt="location image" className="location--image"/>
-          <h3 className="location--title">{title}</h3> 
-          <p className="location--place">{location}</p>
-        </div>
-      )}
-    </div>
+      <h1 className="locations--header">Locations</h1>
+      <div className="locations-container">
+        {locations.map(({ cover, title, location }) => (
+
+            <div className={locartionClassName = setClassName()}>
+              <img
+                src={cover}
+                alt={`location-${animateNumber}`}
+                className="location--image"
+              />
+              <h3 className="location--title">{title}</h3>
+              <p className="location--place">{location}</p>
+            </div>
+
+        ))}
+      </div>
     </>
   );
 }
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import logo from '../images/logo.png'
 import { NavLink } from 'react-router-dom';
 import '../styles/navBar.scss'
 
+
+function StickyNavBar() {
+  useEffect(() => {
+    const navBar = document.querySelector('nav');
+    const scrollWatch = document.createElement('div');
+
+    scrollWatch.setAttribute('data-scroll-watcher', '');
+    navBar.before(scrollWatch);
+
+    const scrollObserver = new IntersectionObserver((entries) =>{
+      // the second parameter in the toggle when it's false it removes the class and when it's true it adds the class
+      navBar.classList.toggle('navBar__sticking', !entries[0].isIntersecting) // if it's not intersecting then add the class and remove it when it's intersecrion
+    })
+    scrollObserver.observe(scrollWatch)
+  }, []);
+
+  return null; // This component doesn't render anything, so return null.
+}
+
+
 function NavBar() {
+  StickyNavBar()
   const handelClick = ()=>{
     SetClassState(!classState)
   }
