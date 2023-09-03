b93aeb43074550ea61858e363c0e3079718626b3
amine abaidi <amineabaidi546@gmail.com>
2023-09-03 14:42:58 +0100

    Fix the shaking bug and the code is react best practice

--- src/components/Dishes.js
+++ src/components/Dishes.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React from "react";
 
-function Dishes({description, price, dish, key, imagURL, ShowDishInfo, setChosenCat}) {
+function Dishes({
+  description,
+  price,
+  dish,
+  imagURL,
+  ShowDishInfo,
+  hoveredElContent,
+  handelMouseLeave,
+}) {
   return (
-    <div className='SelectedImages-container' data-description={description} data-price={price} data-dish={dish} key={key} 
-          onMouseEnter={(event) => ShowDishInfo(event, setChosenCat)}
-        >
-         <div className='SelectedImages__bgColor'></div>
-  
-          <img src={imagURL} alt={dish} className='SelectedImages--DishImage'  key={dish}/>
-        </div>
-  )
+    <div
+      className="SelectedImages-container"
+      data-dish={dish}
+      onMouseEnter={(event) =>
+        ShowDishInfo(event.currentTarget, description, price, dish, imagURL)
+      }
+      onMouseLeave={(event) => handelMouseLeave(event.currentTarget)}
+    >
+      {hoveredElContent}
+      <div className="SelectedImages__bgColor"></div>
+
+      <img
+        src={imagURL}
+        alt={dish}
+        className="SelectedImages--DishImage"
+        key={dish}
+      />
+    </div>
+  );
 }
 
-export default Dishes
+export default Dishes;
--- src/components/ShowSelectedCategory.js
+++ src/components/ShowSelectedCategory.js
@@ -1,68 +1,83 @@
-import Dishes from "./Dishes"
+import Dishes from "./Dishes";
+import { useState } from "react";
 
-const smallerDishes = (Hovered)=>{
-  const dishes = document.querySelectorAll('.SelectedImages-container')
+const smallerDishes = (Hovered) => {
+  const dishes = document.querySelectorAll(".SelectedImages-container");
   dishes.forEach((dish) => {
     // if it's not the hovered element then make it smaller
-    if(dish.dataset.dish !== Hovered.dataset.dish){
-      dish.classList.toggle('SelectedImages__Smallerscal')
+    if (dish.dataset.dish !== Hovered.dataset.dish) {
+      dish.classList.toggle("SelectedImages__Smallerscal");
     }
-  })
-}
-
-const ShowDishInfo = (e, setChosenCat) => {
-  const HoveredElement = e.currentTarget;
-    const description = HoveredElement.dataset.description
-    const price = HoveredElement.dataset.price
-    const dishName = HoveredElement.dataset.dish
-    const image = HoveredElement.querySelector('img').src
-    const BackgoundCercle = HoveredElement.querySelector('.SelectedImages__bgColor')
-    const Btn = document.createElement('button')
-    const DishInfoContainer = document.createElement('div')
-    DishInfoContainer.classList.add('SelectedImages--DishInfo')
-    Btn.classList.add('SelectedImages--DishInfo-Btn')
-    Btn.textContent = 'Add'
-    DishInfoContainer.innerHTML = `<p>${description === undefined ? '' : description}</p>`
-    HoveredElement.appendChild(DishInfoContainer)
-    DishInfoContainer.appendChild(Btn)
-    BackgoundCercle.style.display = 'none';
-    HoveredElement.style.backgroundColor = '#FE5D03';
-    
-    // make the other dishes smaller campared to the hovered dish
-    smallerDishes(HoveredElement)
-    
-    Btn.addEventListener('click', ()=>{
-      const newDish = {
-        name: dishName,
-        price: price,
-        image: image,
-        quantity: 1,
-    };
-  
-    setChosenCat(prevChosenCat => [...prevChosenCat, newDish]);
-    })
-
-    HoveredElement.addEventListener('mouseleave', () => {
-
-      smallerDishes(HoveredElement)
-      HoveredElement.style.backgroundColor = '';
-      BackgoundCercle.style.display = 'block';
-        Btn.remove()
-       DishInfoContainer.remove()
-    } )
-
-
-}
-
+  });
+};
 
 const ShowSelectedCategory = ({ SelcetCategory, setChosenCat }) => {
+  const addToCart = (dishName, price, image) => {
+    const newDish = {
+      name: dishName,
+      price: price,
+      image: image,
+      quantity: 1,
+    };
 
+    setChosenCat((prevChosenCat) => [...prevChosenCat, newDish]); // adding the new chosen dish to the carts
+  };
+
+  const ShowDishInfo = (
+    HoveredElement,
+    description,
+    price,
+    dishName,
+    image
+  ) => {
+    const BackgoundCercle = HoveredElement.querySelector(
+      ".SelectedImages__bgColor"
+    );
+
+    setContent(
+      <div className="SelectedImages--DishInfo">
+        <p>{description === undefined ? "" : description}</p>
+        <button
+          className="SelectedImages--DishInfo-Btn"
+          onClick={() => addToCart(dishName, price, image)}
+        >
+          Add
+        </button>
+      </div>
+    );
+
+    BackgoundCercle.setAttribute("id", "d-none");
+    HoveredElement.style.backgroundColor = "#FE5D03";
+    // make the other dishes smaller campared to the hovered dish
+    smallerDishes(HoveredElement);
+  };
+
+
+  const handelMouseLeave = (HoveredElement) => {
+   smallerDishes(HoveredElement);
+    HoveredElement.style.backgroundColor = "";
+    HoveredElement.querySelector(".SelectedImages__bgColor").setAttribute(
+      "id",
+      "d-block"
+    );
+    setContent(null);
+  };
+
+  const [hoveredElContent, setContent] = useState(null);
 
   return (
-    <div className='SelectedImages'>
-      {
-      SelcetCategory.id.map(({ imagURL, dish, description, price, key}) => (
-        <Dishes imagURL={imagURL}  dish={dish} description = {description} price={price} key={key} ShowDishInfo={ShowDishInfo} setChosenCat={setChosenCat}/>
+    <div className="SelectedImages">
+      {SelcetCategory.id.map(({ imagURL, dish, description, price, key }) => (
+        <Dishes
+          imagURL={imagURL}
+          dish={dish}
+          description={description}
+          price={price}
+          key={key}
+          ShowDishInfo={ShowDishInfo}
+          hoveredElContent={hoveredElContent}
+          handelMouseLeave={handelMouseLeave}
+        />
       ))}
     </div>
   );
